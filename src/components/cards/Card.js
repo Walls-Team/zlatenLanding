e47ab0be78faf45fa3b8c652543a52e1c916aaaf
Iveth <ivethanselmi@gmail.com>
2023-03-14 14:39:02 -0400

    fix the color of the chart label according to video, audio, etc

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,51 +1,53 @@
 import { text } from '@fortawesome/fontawesome-svg-core'
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../css/Card.scss'
 
-function Card({title, text, imageSource, cliente, format,type,view,id,link="/", download, preview}) {
+function Card({ title, text, imageSource, cliente, format, type, view, id, link = "/", download, preview }) {
   const [info, setInfo] = useState()
-  
+
   const types = {
-    audio : {color: "#EBFFFF"},
-    video : { color: "#EFD9CE"},
-    twoD : { color: "#C8E2F5"},
-    creative : {color: "#FFFAC9"},
-    digital : {color:"#DFF0E8"},
-    threeD : {color: "#EBE9F5"},
+    audio: { color: "#EBFFFF" },
+    video: { color: "#EFD9CE" },
+    twoD: { color: "#C8E2F5" },
+    creative: { color: "#FFFAC9" },
+    digital: { color: "#DFF0E8" },
+    threeD: { color: "#EBE9F5" },
   }
   useEffect(() => {
     setInfo(types[type])
+    console.log('//////////info ', info)
     // eslint-disable-next-line
-  },[title])
+  }, [title])
 
   return (
-    <div className = "card" key={id}>
+    <div className="card" key={id}>
       <div id='container'>
-      <div id='preview-client'>{preview}</div>
-       <img src={imageSource} alt="" className='product--image'/>
-       </div>
-       <Link to={link}>
-      <div className ="card-body">
-           <h4>{title}</h4>
-           <h3>{cliente}</h3>
-           <p style={{
+        <div id='preview-client'>{preview}</div>
+        <img src={imageSource} alt="" className='product--image' />
+      </div>
+      <Link to={link}>
+        <div className="card-body">
+          <h4>{title}</h4>
+          <h3>{cliente}</h3>
+          <p style={{
             fontSize: '16px',
             lineHeight: '24px',
             fontWeight: '400',
             color: '#707070',
-           }}
-           >{text}
-            </p>
-           <div className='contentElementsCard'>
-            <span className='format'>{format}</span>
+          }}
+          >{text}
+          </p>
+          <div className='contentElementsCard'>
+            <span className='format' style={{ background: info?.color || '#EBFFFF' }}>{format}</span>
             {download}
-           </div>
-           <span className='view'>{view}</span>
-      </div>
+          </div>
+          <span className='view'>{view}</span>
+        </div>
       </Link>
     </div>
 
   )
 }
+
 export default Card
